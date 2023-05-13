bb5c44bc9b788c1046c23a96ffb96b5899b5cf5b
d2n0s4ur <njh0625@gmail.com>
2023-05-14 01:02:53 +0900

    1

--- routes/card.js
+++ routes/card.js
@@ -5,6 +5,6 @@ const cardController = require('../controllers/cardController');
 router.post('/add', cardController.addCard);
 router.get('/list', cardController.listCard);
 router.get('/mycard', cardController.MyCard);
-router.delete('/delete', cardController.detailCard);
+router.delete('/delete', cardController.deleteCard);
 
 module.exports = router;
